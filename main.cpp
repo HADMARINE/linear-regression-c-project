b6b9abe0ea0294ee0bec40da9fc1e1f435a41fde
HADMARINE <joonlee031@live.com>
2019-11-16 22:29:21 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+//
+//  main.cpp
+//  linear-regression-c-project
+//
+//  Created by 이호준 on 2019/11/16.
+//
+
+#include "main.hpp"
---
+++ main.hpp
@@ -0,0 +1,13 @@
+//
+//  main.hpp
+//  linear-regression-c-project
+//
+//  Created by 이호준 on 2019/11/16.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
